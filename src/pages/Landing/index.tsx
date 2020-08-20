5f9d255686ac00df11de788dce944d3e81f24f92
Natalialimac <nataliadelimac@outlook.com>
2020-08-20 19:42:46 -0300

    Pag principal ok

--- src/pages/Landing/index.tsx
+++ src/pages/Landing/index.tsx
@@ -11,7 +11,9 @@ function Landing(){
            <div id="pages-landing-content">
                 <div className="logo-container">
                     <img src={logo} alt="logo" className="hero-image"/>
-                    <h2>Serviços Administrativos</h2>
+                    <h1>A.N.C</h1>
+                    <h2>home adm</h2>
+                    {/* <h2>Serviços Administrativos</h2> */}
                 </div>
 
                 <div className="buttons-container">
